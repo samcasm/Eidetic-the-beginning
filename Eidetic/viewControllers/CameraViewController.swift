fb6bfa41c4a50959b38aa8d6fbd24b0a44f68926
hussainaww <hussainaww@gmail.com>
2019-01-10 03:59:15 -0800

    added camera functionality

--- Eidetic/viewControllers/CameraViewController.swift
+++ Eidetic/viewControllers/CameraViewController.swift
@@ -7,82 +7,92 @@
 //
 
 import UIKit
-import AVFoundation
-import PhotosUI
+import Photos
 
-
-class CameraViewController : UIViewController, AVCapturePhotoCaptureDelegate  {
+class CameraViewController : UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate  {
     
-    @IBOutlet weak var previewView: UIView!
     @IBOutlet weak var captureImageView: UIImageView!
+    var imagePicker: UIImagePickerController!
+    var lastImageAsset: PHAsset!
     
-    var captureSession: AVCaptureSession!
-    var stillImageOutput: AVCapturePhotoOutput!
-    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
-    
-    @IBAction func didTakePhoto(_ sender: Any) {
-        let settings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])
-        stillImageOutput.capturePhoto(with: settings, delegate: self)
-        
+    enum ImageSource {
+        case photoLibrary
+        case camera
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        self.captureSession.stopRunning()
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        // Setup your camera here...
-        
-        captureSession = AVCaptureSession()
-        captureSession.sessionPreset = .medium
-        
-        guard let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
-            else {
-                print("Unable to access back camera!")
-                return
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            selectImageFrom(.photoLibrary)
+            return
         }
-        
-        do {
-            let input = try AVCaptureDeviceInput(device: backCamera)
-            stillImageOutput = AVCapturePhotoOutput()
+        selectImageFrom(.camera)
+    }
+    
+    
+    func selectImageFrom(_ source: ImageSource){
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        switch source {
+        case .camera:
+            imagePicker.sourceType = .camera
+        case .photoLibrary:
+            imagePicker.sourceType = .photoLibrary
+        }
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    //MARK: - Add image to Library
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            // we got back an error!
+            showAlertWith(title: "Save error", message: error.localizedDescription)
+        } else {
+            showAlertWith(title: "Saved!", message: "Your image has been saved to your photos.")
+            let fetchOptions = PHFetchOptions()
+            fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+            fetchOptions.fetchLimit = 1
             
-            stillImageOutput = AVCapturePhotoOutput()
+            let fetchResult = PHAsset.fetchAssets(with: .image, options: fetchOptions)
+            if (fetchResult.firstObject != nil)
+            {
+                lastImageAsset = fetchResult.firstObject
+            }else{
+                lastImageAsset = nil
+            }
             
-            if captureSession.canAddInput(input) && captureSession.canAddOutput(stillImageOutput) {
-                captureSession.addInput(input)
-                captureSession.addOutput(stillImageOutput)
-                setupLivePreview()
-            }
-        }
-        catch let error  {
-            print("Error Unable to initialize back camera:  \(error.localizedDescription)")
+            imagePicker.dismiss(animated: true, completion: nil)
+            
+            self.performSegue(withIdentifier: "cameraPhotoDetailsSegue", sender: self)
         }
     }
     
-    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
-        
-        guard let imageData = photo.fileDataRepresentation()
-            else { return }
-        
-        let image = UIImage(data: imageData)
-        captureImageView.image = image
+    func showAlertWith(title: String, message: String){
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
     
-    func setupLivePreview() {
-        
-        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        
-        videoPreviewLayer.videoGravity = .resizeAspect
-        videoPreviewLayer.connection?.videoOrientation = .portrait
-        previewView.layer.addSublayer(videoPreviewLayer)
-        
-        DispatchQueue.global(qos: .userInitiated).async { //[weak self] in
-            self.captureSession.startRunning()
-            DispatchQueue.main.async {
-                self.videoPreviewLayer.frame = self.previewView.bounds
-            }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[.originalImage] as? UIImage else {
+            print("Image not found!")
+            return
         }
+        UIImageWriteToSavedPhotosAlbum(selectedImage, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+        
+        
     }
+ 
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "cameraPhotoDetailsSegue"{
+            guard let destination = segue.destination as? AssetViewController
+                else { fatalError("unexpected view controller for segue") }
+            
+            destination.asset = lastImageAsset
+        }
+        
+    }
+    
 }
