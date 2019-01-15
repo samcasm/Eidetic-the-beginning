1916e7348704ed8d440e669b50dbebdc1b9f99ca
user147964 <user147964@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-01-14 21:16:33 -0800

    refactor after delete folder popup addition

--- Eidetic/extensions/UIViewController.swift
+++ Eidetic/extensions/UIViewController.swift
@@ -124,17 +124,5 @@ extension UIViewController {
         return selectedAssetsIds
     }
     
-    func showAlertButtonTapped() {
-        
-        // create the alert
-        let alert = UIAlertController(title: "UIAlertController", message: "Would you like to continue learning how to use iOS alerts?", preferredStyle: UIAlertController.Style.alert)
-        
-        // add the actions (buttons)
-        alert.addAction(UIAlertAction(title: "Continue", style: UIAlertAction.Style.default, handler: nil))
-        alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil))
-        
-        // show the alert
-        self.present(alert, animated: true, completion: nil)
-    }
     
 }
