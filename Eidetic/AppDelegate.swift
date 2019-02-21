570cb79687b82885fb9858d42656a1ba004536fe
user147964 <user147964@ny011.local>
2019-02-20 17:58:03 -0800

    push notification test app delegate

--- Eidetic/AppDelegate.swift
+++ Eidetic/AppDelegate.swift
@@ -8,14 +8,45 @@
 
 import UIKit
 import Foundation
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     var window: UIWindow?
-
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .sound])
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("print")
+//        let subscription = CKQuerySubscription(recordType: "GlobalNotification", predicate: NSPredicate(format: "TRUEPREDICATE"), options: .firesOnRecordCreation)
+//
+//        let info = CKNotificationInfo()
+//        info.alertBody = "A new notification has been posted!"
+//        info.shouldBadge = true
+//        info.soundName = "default"
+//
+//        subscription.notificationInfo = info
+//
+//        CKContainer.default().publicCloudDatabase.save(subscription, completionHandler: { subscription, error in
+//            if error == nil {
+//                // Subscription saved successfully
+//            } else {
+//                // An error occurred
+//            }
+//        })
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: { authorized, error in
+            if authorized {
+                application.registerForRemoteNotifications()
+            }
+        })
         
         // Override point for customization after application launch.
         let defaults = UserDefaults.standard
