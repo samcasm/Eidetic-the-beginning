47634b0faec98ac048545b4c3ef1f0f36dffde51
user147964 <user147964@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-03-01 15:08:16 -0800

    Pods directory deleted

--- Pods/Target Support Files/EEZoomableImageView/EEZoomableImageView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_EEZoomableImageView : NSObject
-@end
-@implementation PodsDummy_EEZoomableImageView
-@end
--- Pods/Target Support Files/MaterialShowcase/MaterialShowcase-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MaterialShowcase : NSObject
-@end
-@implementation PodsDummy_MaterialShowcase
-@end
